3a041970cc278bedaf570d0249b464c03ec6c924
samhuynhle <samhuynhle@gmail.com>
2019-09-24 16:38:06 -0700

    jdoodle access completed only for python

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,8 @@
 const controller = require('./controller.js');
 
 module.exports = (app)=>{
+    app.get('/questions', controller.find_questions)
+    app.post('/questions', controller.create_question)
     app.post('/game', controller.create_game)
     app.post('/game/check', controller.check_code)
     // app.put('/game', controller.new_question)
