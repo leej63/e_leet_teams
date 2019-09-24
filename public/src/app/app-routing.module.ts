c74c3531f8f0ccf03b63a963ee72340d033ce502
Jonathan Lee <jonathan_496@hotmail.com>
2019-09-24 13:37:13 -0700

    update

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { GameComponent } from './game/game.component';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'game', component: GameComponent },
+  { path: '', component: LandingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ public/src/app/game/game.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+
+  constructor(private _httpService: HttpService, private _route: ActivatedRoute, private _router: Router) { }
+
+  ngOnInit() {
+  }
+
+}
