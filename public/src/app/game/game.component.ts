456af1856e1b96b8105536bb94638f208a0b219f
Jonathan Lee <jonathan_496@hotmail.com>
2019-09-26 15:02:15 -0700

    implementing timer

--- public/src/app/game/game.component.ts
+++ public/src/app/game/game.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { GameService } from '../game.service';
+import { delay } from 'q';
 
 
 @Component({
@@ -13,9 +14,13 @@ export class GameComponent implements OnInit {
 
   message = '';
   messages = [];
+  seconds = 0;
+  minutes = 25;
+  counter = 1500;
 
   constructor(private _httpService: HttpService, private _route: ActivatedRoute, private _router: Router , private gameService: GameService) { }
   ngOnInit() {
+    this.countdownTimer();
 
     this.gameService
       .addMessage()
@@ -32,5 +37,23 @@ export class GameComponent implements OnInit {
     this.message = '';
   }
 
-  
+  async countdownTimer() {
+    for (let i = 1500; i > 0; i--){
+      await delay(1000);
+      this.counter = this.counter -1;
+      // console.log(this.counter)
+      if(this.counter == 0){
+        this._router.navigate(['/']);                       // PLACEHOLDER - need to have redirect to win or loss page
+        return
+      }
+      if(this.seconds == 0){
+        this.seconds = 59;
+        this.minutes = this.minutes - 1;
+      }
+      else{
+        this.seconds = this.seconds - 1;
+      }
+    }
+  }
+
 }
